e206d17b00c8983fd6bbfbd74c36aad2da18b2b5
Mednasa <girgin183@gmail.com>
2024-02-09 10:49:11 +0300

    Kahve Hazırla Done v1.1

--- src/Kafe.java
+++ src/Kafe.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Kafe {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         KahveHazırla kahve = new KahveHazırla();
 
--- src/KahveHazırla.java
+++ src/KahveHazırla.java
@@ -2,12 +2,37 @@ import java.util.Scanner;
 
 public class KahveHazırla {
     Scanner oku = new Scanner(System.in);
+    Scanner okuStr=new Scanner(System.in);
 
     public KahveHazırla() {
     }
 
-    public void kahveyiHazırla(int choice) {
-        System.out.println("---kahveler---" + " seçtiniz.");
+    public void kahveyiHazırla(int choice) throws InterruptedException {
+        switch (choice){
+            case 1:
+                System.out.println(Kahveler.AMERICANO + " seçtiniz.");break;
+            case 2:
+                System.out.println(Kahveler.ESPRESSO + " seçtiniz.");break;
+            case 3:
+                System.out.println(Kahveler.CAPPUCINO + " seçtiniz.");break;
+            case 4:
+                System.out.println(Kahveler.LATTE + " seçtiniz.");break;
+            case 5:
+                System.out.println(Kahveler.MOCHA + " seçtiniz.");break;
+            case 6:
+                System.out.println(Kahveler.MACCHIATO + " seçtiniz.");break;
+            case 7:
+                System.out.println(Kahveler.AFFOGATO + " seçtiniz.");break;
+            case 8:
+                System.out.println(Kahveler.BUZLU_KAHVE + " seçtiniz.");break;
+            case 9:
+                System.out.println(Kahveler.FRAPPUCCINO + " seçtiniz.");break;
+            case 10:
+                System.out.println(Kahveler.IRISH + " seçtiniz.");break;
+            default:
+                System.out.println("Geçersiz Kahve seçtiniz");
+        }
+
         if (choice == Kahveler.FRAPPUCCINO.ordinal() || choice == Kahveler.BUZLU_KAHVE.ordinal()) {
             System.out.println("Sıcak bir yaz gününde serinlemenin en iyi yolu… Mükemmel karar!");
         }
@@ -16,55 +41,87 @@ public class KahveHazırla {
         do {
             isTrue = true;
             System.out.println("- Lütfen aşağıdaki menüden kahvenizin boyutunu (size) seçiniz…"+
-                    "1. Small"+
-                    "2. Medium"+
-                    "3. Large"+
-                    "4. XLarge");
+                    " \n1. Small"+
+                    " \n2. Medium"+
+                    " \n3. Large"+
+                    " \n4. XLarge");
             size = oku.nextInt();
-            if (size == Boyutlar.SMALL.ordinal()){
-                System.out.println("{size} {kahve}'niz hazırlanıyor…");
-            } else if (size == Boyutlar.MEDIUM.ordinal()) {
-                System.out.println("{size} {kahve}'niz hazırlanıyor…");
-            } else if (size == Boyutlar.LARGE.ordinal()) {
-                System.out.println("{size} {kahve}'niz hazırlanıyor…");
-            }else if (size == Boyutlar.XLARGE.ordinal()){
-                System.out.println("{size} {kahve}'niz hazırlanıyor…");
-            }
-            else {
-                isTrue = false;
+
+
+            switch (size){
+                case 1:System.out.println(Boyutlar.SMALL+" kahveniz hazırlanıyor…");
+                    for (int i = 0; i < 20; i++) {
+                        Thread.sleep(100);
+                        System.out.print(">");
+                    }break;
+                case 2:System.out.println(Boyutlar.MEDIUM+" kahveniz hazırlanıyor…");
+                    for (int i = 0; i < 20; i++) {
+                        Thread.sleep(100);
+                        System.out.print(">");
+                    }break;
+                case 3:System.out.println(Boyutlar.LARGE+" kahveniz hazırlanıyor…");
+                    for (int i = 0; i < 20; i++) {
+                        Thread.sleep(100);
+                        System.out.print(">");
+                    }break;
+                case 4:System.out.println(Boyutlar.XLARGE+" kahveniz hazırlanıyor…");
+                    for (int i = 0; i < 20; i++) {
+                        Thread.sleep(100);
+                        System.out.print(">");
+                    }break;
+                default:
+                    System.out.println("Geçersiz kahve boyu tekrar deneyin");
+                    isTrue=false;
             }
 
         }while (!isTrue);
-
-
 
         String sut;
         int kacSeker;
 
         do {
-            System.out.print("Süt eklemek ister misiniz: ");
-            sut = oku.nextLine();
-        } while (!sut.equalsIgnoreCase("evet") || !sut.equalsIgnoreCase("hayır"));
+            System.out.print("\nSüt eklemek ister misiniz: ");
+            sut = okuStr.nextLine();
+        } while (!sut.equalsIgnoreCase("evet") && !sut.equalsIgnoreCase("hayır"));
 
-        if (sut.equalsIgnoreCase("evet"))
+        if (sut.equalsIgnoreCase("evet")) {
             System.out.println("Süt ekleniyor");
-        else if (sut.equalsIgnoreCase("hayır"))
+            for (int i = 0; i < 20; i++) {
+                Thread.sleep(100);
+                System.out.print(">");
+            }
+        }else if (sut.equalsIgnoreCase("hayır")) {
             System.out.println("Süt eklenmiyor");
+
+        }
         String seker;
 
         do {
-            System.out.print("Şeker ister misiniz?(Evet veya Hayır olarak cevaplayınız) ");
-            seker = oku.nextLine();
-        } while (!seker.equalsIgnoreCase("evet") || !seker.equalsIgnoreCase("hayır"));
+            System.out.print("\nŞeker ister misiniz?(Evet veya Hayır olarak cevaplayınız) ");
+            seker = okuStr.nextLine();
+        } while (!seker.equalsIgnoreCase("evet") && !seker.equalsIgnoreCase("hayır"));
 
         if (seker.equalsIgnoreCase("evet")) {
             System.out.println("Kaç şeker istersiniz? ");
             kacSeker = oku.nextInt();
             oku.nextLine();
             System.out.println(kacSeker + " şeker ekleniyor");
+            for (int i = 0; i < 20; i++) {
+                Thread.sleep(100);
+                System.out.print(">");
+            }
         } else if (seker.equalsIgnoreCase("hayır")) {
             System.out.println("Şeker eklenmiyor");
+            System.out.println();
         }
+
+        System.out.println("\nKahveniz hazırlanıyor...");
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(100);
+            System.out.print(">");
+
+        }
+        System.out.println("\nKahveniz hazır Afiyet Olsun!.....");
     }
 
 
